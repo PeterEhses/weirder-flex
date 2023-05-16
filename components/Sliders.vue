17a7ec3148156980565937de623c84dc252b6d80
Peter Ehses <ehsesp@hochschule-trier.de>
2023-05-16 17:26:02 +0200

    change gamepad version

---
+++ components/Sliders.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <input type="range" name="weight" id="Sl-Weight" />
+    <input type="range" name="width" id="Sl-Width" />
+    <input type="range" name="slant" id="Sl-Slant" />
+  </div>
+</template>
+
+<script>
+import {createJoymap, createQueryModule} from 'joymap';
+import { join, compact, forEach } from 'lodash/fp';
+
+function setupModule(joymap, padId) {
+  const m = createQueryModule({ padId });
+  joymap.addModule(m);
+  };
+
+
+export default {
+  methods: {
+    receiveGamepadEvent(e) {
+      console.log(
+        "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+        e.gamepad.index,
+        e.gamepad.id,
+        e.gamepad.buttons.length,
+        e.gamepad.axes.length
+      );
+    },
+  },
+  mounted() {
+    window.addEventListener(
+      "gamepadconnected",
+      this.receiveGamepadEvent
+    );
+
+
+// Initial joymap setup
+const joymap = createJoymap({
+  onPoll() {
+    const unusedIds = joymap.getUnusedPadIds();
+
+    if (unusedIds.length > 0) {
+      forEach((padId) => setupModule(joymap, padId), unusedIds);
+    }
+
+    forEach((module) => {
+      if( module.getPadId() === 'Unknown Gamepad (Vendor: e502 Product: bbab)'){
+        console.log([...module.getAllSticks()['L'].value, module.getAllButtons()['L2'].value])
+      }
+    }, joymap.getModules());
+  },
+});
+
+joymap.start();
+
+  },
+  unmounted() {
+    window.removeEventListener("gamepadconnected", this.receiveGamepadEvent);
+  },
+};
+</script>
+
+<style></style>
